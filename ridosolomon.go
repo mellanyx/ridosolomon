f029b1a437c0f2574080391a5abc96072cdb3553
mellanyx <starik0v2000@yandex.ru>
2021-11-06 01:06:22 +0300

    added func fileToArByte

--- ridosolomon.go
+++ ridosolomon.go
@@ -1,7 +1,22 @@
 package ridosolomon
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
 
-func Hello(name string) string  {
-	return fmt.Sprintf("Hello %v.", name)
+func Hello(name string) {
+	fmt.Sprintf("Hello %v, i created by Alexandr Starikov!", name)
+}
+
+func fileToArByte(filename string) ([]byte, error) {
+	// return []byte, nil if status OK
+	// return nil, err if status FAIL
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, errors.Unwrap(err)
+	}
+
+	return b, nil
 }
